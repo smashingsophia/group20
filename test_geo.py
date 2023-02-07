022166bae80e329bdfb3ada1a08c66aaaad75aa9
sophia <1224275483@qq.com>
2023-02-07 12:24:28 +0000

    test

--- test_geo.py
+++ test_geo.py
@@ -1,5 +1,5 @@
 from floodsystem.station import MonitoringStation
-from floodsystem.geo import rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius
+from floodsystem.geo import rivers_with_station, stations_by_distance, stations_by_river, stations_within_radius, rivers_by_station_number
 
 def test_stations_by_distance():
     Test_station_list= []
@@ -88,3 +88,23 @@ def test_stations_by_river():
     x = stations_by_river(Test_station_list)
     
     assert type(x) == dict
+    
+def test_rivers_by_station_number():
+    Test_station_list= []
+    
+    for i in range(50):
+        s_id = "test-s-id"
+        m_id = "test-m-id"
+        label = "some station"
+        coord = (i, 4.0)
+        trange = (-2.3, 3.4445)
+        river = "River X"
+        town = "My Town"
+        s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+        
+        Test_station_list.append(s)
+        
+    x = rivers_by_station_number(Test_station_list,1)
+    
+    assert type(x) == list
+test_rivers_by_station_number()
